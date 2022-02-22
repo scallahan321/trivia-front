ed75bcacb2cccb687d47bd5d6ae26c5c763deff8
Sean Callahan <sean@Seans-MacBook-Air.local>
2022-02-21 18:43:05 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios'
 
 function App() {
+  const [stuff, setStuff] = useState({})
+
+  useEffect(()=>{
+    axios.get('http://localhost:8000/questions').then(response => {
+      console.log("SUCCESS", response)
+      setStuff(response)
+    }).catch(error => {
+      console.log(error)
+    })
+
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {stuff.data}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
