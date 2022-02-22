534b3abc0ce7f6bf93fba8a9d733f415577e2853
Sean Callahan <sean@Seans-MacBook-Air.local>
2022-02-22 12:07:32 -0700

    work

--- src/App.js
+++ src/App.js
@@ -2,55 +2,44 @@ import './App.css';
 import React, {useEffect, useState} from 'react';
 import axios from 'axios'
 
+
 function App(){
   
-  const [question, setQuestion] = useState('');
-  const [one, setOne] = useState('');
-  const [two, setTwo] = useState('');
-  const [three, setThree] = useState('');
-  const [four, setFour] = useState('');
-  const [correct, setCorrect] = useState('');
+  //const [questions, SetQuestion] = useState(JSON.parse(sessionStorage.getItem('questions')));
+  //const [current, SetCurrent] = useState(0)
+  const questions = JSON.parse(sessionStorage.getItem('questions'));
+  const [count, SetCount] = useState(0);
+  const label = ['q1', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7', 'q8', 'q9', 'q10']
 
- /* constructor(props) {
-    super(props);
-    this.state = {
-      question_text: "",
-      answer1: ""
-    };
-  }*/
+  function update() {
+    if (count < 9) {
+      SetCount(count + 1)
+    }
+  }
 
   useEffect(()=>{
       axios.get('http://localhost:8000/questions').then(response => {
-      //console.log('success')
-      //this.useState({'question_text':response.question_text, "answer1":response.answer1})
-      const data = response.data
-      setQuestion(data.question)
-      setOne(data.one)
-      setTwo(data.two)
-      setThree(data.three)
-      setFour(data.four)
-      setCorrect(data.correct)
-
+  
+      const data = JSON.stringify(response.data);
+      sessionStorage.setItem('questions', data);
+      
     }).catch(error => {
       console.log(error)
     })
 
   }, [])
 
+  //console.log(questions[query].question)
+
 
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>{question} </p>
-        <ul>
-          <li>{one}</li>
-          <li>{two}</li>
-          <li>{three}</li>
-          <li>{four}</li>
-        </ul>
-    
-        
+      <button onClick={update}>
+        Click me
+      </button>
+      <p>{questions[label[count]].question}</p>
       </header>
     </div>
   )
